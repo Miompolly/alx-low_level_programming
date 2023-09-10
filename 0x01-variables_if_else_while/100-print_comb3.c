a92105b6d65d8b324d905246f0afa3b891bc215b
Miompolly <57joel39@gmail.com>
2023-09-10 01:36:54 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the digits of the decimal number system using putchar
+ * Return: 0 when code executes succesfully
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 8 && b == 9)
+			{
+				break;
+			}
+				putchar(',');
+				putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
